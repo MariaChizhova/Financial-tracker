bc37c864a2649e58f508f526f3cb756cce3c10ae
SorokinSemyon <semon.sorokin@gmail.com>
2020-10-16 04:20:15 +0300

    Models

--- financial_tracker/main/models.py
+++ financial_tracker/main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Currency(models.Model):
+    name = models.TextField()
+    rate = models.FloatField()
+
+
+class Purse(models.Model):
+    name = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    name = models.TextField()
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+
+
+class Transaction(models.Model):
+    purse = models.ForeignKey(Purse, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    merchant = models.TextField()
