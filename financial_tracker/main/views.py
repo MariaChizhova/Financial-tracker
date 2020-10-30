8cee1839476240f9aa7c51e440e8a0e3628f6ccc
Maria <mariachizhova17@gmail.com>
2020-10-30 21:55:45 +0300

    Changed size of the table

--- financial_tracker/main/views.py
+++ financial_tracker/main/views.py
@@ -42,15 +42,15 @@ def choose_purse(request):
 
 def display_table(request):
     List = [
-        {'id': 1, 'date': "05/09/1842", 'merchant': "APPLE.COM", 'amount': "20", 'category': "TECH"},
-        {'id': 2, 'date': "18/02/2000", 'merchant': "APPLE.COM", 'amount': "212", 'category': "TECH"},
-        {'id': 3, 'date': "23/05/1945", 'merchant': "YANDEX LAVKA", 'amount': "3", 'category': "FOOD"},
-        {'id': 4, 'date': "29/04/2011", 'merchant': "AMAZON.CO.UK", 'amount': "567", 'category': "-"},
-        {'id': 5, 'date': "29/04/2011", 'merchant': "METRO", 'amount': "8", 'category': "TRANSPORT"},
-        {'id': 6, 'date': "14/07/2020", 'merchant': "YANDEX TAXI", 'amount': "35", 'category': "TRANSPORT"},
-        {'id': 7, 'date': "01/06/2015", 'merchant': "BOLT", 'amount': "24.34", 'category': "TRANSPORT"},
-        {'id': 8, 'date': "02/07/2013", 'merchant': "SAMOKAT", 'amount': "34.11", 'category': "FOOD"},
-        {'id': 9, 'date': "01/06/2005", 'merchant': "GOOGLE", 'amount': "456", 'category': "-"},
-        {'id': 10, 'date': "18/", 'merchant': "GQT Wabash Landing 9", 'amount': "10", 'category': "CINEMA"},
+        {'date': "05/09/1842", 'merchant': "APPLE.COM", 'amount': "20", 'category': "TECH"},
+        {'date': "18/02/2000", 'merchant': "APPLE.COM", 'amount': "212", 'category': "TECH"},
+        {'date': "23/05/1945", 'merchant': "YANDEX LAVKA", 'amount': "3", 'category': "FOOD"},
+        {'date': "29/04/2011", 'merchant': "AMAZON.CO.UK", 'amount': "567", 'category': "-"},
+        {'date': "29/04/2011", 'merchant': "METRO", 'amount': "8", 'category': "TRANSPORT"},
+        {'date': "14/07/2020", 'merchant': "YANDEX TAXI", 'amount': "35", 'category': "TRANSPORT"},
+        {'date': "01/06/2015", 'merchant': "BOLT", 'amount': "24.34", 'category': "TRANSPORT"},
+        {'date': "02/07/2013", 'merchant': "SAMOKAT", 'amount': "34.11", 'category': "FOOD"},
+        {'date': "01/06/2005", 'merchant': "GOOGLE", 'amount': "456", 'category': "-"},
+        {'date': "18/10/1000", 'merchant': "GQT Wabash Landing 9", 'amount': "10", 'category': "CINEMA"},
     ]
     return render(request, 'display_table.html', {'tabledata':  simplejson.dumps(List)})
